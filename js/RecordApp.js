7353e4497584900e458e55620a00e1a83232b672
Alicia <wfei@iu.edu>
2019-09-21 13:46:54 -0400

    1

--- js/RecordApp.js
+++ js/RecordApp.js
@@ -1,13 +1,33 @@
 var patientRecordApp = new Vue({
   el: '#patientRecordApp',
   data: {
-    patients: []
+    patients: [],
+    formPatient:{
+       patientGuid:'',
+      firstName: '',
+        lastName:'',
+        sexAtBirth:'',
+        priority:'',
+
+    }
   },
   methods: {
     fetchPatients() {
       fetch('dummy.php')
       .then(response => response.json())
       .then(json => { patientRecordApp.patients = json })
+    },
+    handleCreateRecord(event){
+// fetch(url, {method='post',data:this.formPatient})
+this,formPatient.push(this.formPatient);
+this.formPatient={
+  patientGuid:'',
+ firstName: '',
+   lastName:'',
+   sexAtBirth:'',
+   priority:'',
+
+}
     }
   },
   created() {
